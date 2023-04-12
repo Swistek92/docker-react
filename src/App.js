2925abe8868f8f7cf4fbf7b2b5e50f4f102d19d5
Piotrek <Swistekxd@gmail.com>
2023-04-12 03:23:13 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>i was change</p>
+        <p>i was change on the future branch</p>
         <a
           className='App-link'
           href='https://reactjs.org'
